c40cc7f0e0f8c2c292e21b38807683829218ec6d
Patrick Kirby <pkirbyinatx@gmail.com>
2020-08-16 12:36:03 -0500

    added tweet compenent with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Hello from './sayHello'
+import React from "react";
+import Tweet from "./Tweet";
 
-function App(){
-
-  return(
-    <div>
-      <h1>This is the App component</h1>
-      <Hello />
+function App() {
+  return (
+    <div className="app">
+      <Tweet name="Bob" message="Hi, how is everyone doing!?" likes="7" />
+      <Tweet name="Timmy" message="I can't wait for dinner!" likes="466" />
+      <Tweet name="Jessica" message="hellow world" likes="26" />
     </div>
   );
 }
